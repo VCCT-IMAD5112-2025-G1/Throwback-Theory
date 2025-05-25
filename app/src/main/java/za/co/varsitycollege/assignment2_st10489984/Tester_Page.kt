9a40dd0287a90c61a5dc931ef7223bab34ba3782
ST10489984 <ST10489984@vcconnect.edu.za>
2025-05-25 12:54:45 +0200

    Coded was added to first screen and the second and third screen were created with code in second screen

--- app/src/main/java/za/co/varsitycollege/assignment2_st10489984/Tester_Page.kt
+++ app/src/main/java/za/co/varsitycollege/assignment2_st10489984/Tester_Page.kt
@@ -3,40 +3,28 @@ package za.co.varsitycollege.assignment2_st10489984
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Switch
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Tester_Page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_tester_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val getStartedButton=findViewById<Button>(R.id.getStartedButton)
+        val actualQuizButton=findViewById<Button>(R.id.actualQuizButton)
 
-        val newUserSwitch=findViewById<Switch>(R.id.newUserSwitch)
-
-        getStartedButton?.setOnClickListener {
-            var user:Boolean=newUserSwitch.isChecked
-
-            if (user) {
-                val intent = Intent(this,Tester_Page::class.java)
-                startActivity(intent)
-            } else {
-                val intent = Intent(this,Flashcard_Page::class.java)
-                startActivity(intent)
-            }
-
-            Toast.makeText(this@MainActivity,"Getting ready!", Toast.LENGTH_SHORT).show()
-
+        actualQuizButton.setOnClickListener {
+            Toast.makeText(this,"Goodluck!",Toast.LENGTH_LONG).show()
+            val intent = Intent(this,Flashcard_Page::class.java)
+            startActivity(intent)
         }
     }
 }
